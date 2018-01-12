0678b2d4e11da0ed3ef33406289d0e25f060dc59
alex yungan <yungan@hotmail.es>
2018-01-12 15:17:30 -0500

    db

---
+++ AD/db/chat.sql
@@ -0,0 +1,24 @@
+﻿create table usuario
+(
+id serial Primary key,
+usuario varchar (50),
+contrasena varchar (50)
+)
+
+create table tipo_mensage
+(
+id serial Primary key,
+descripcion varchar (50)
+)
+
+
+create table mensaje
+(
+id serial primary key,
+descripcion varchar(200),
+link varchar(200),
+id_usuario int ,
+id_tipo_mensage int,
+foreign key (id_usuario) references usuario(id),
+foreign key (id_tipo_mensage) references tipo_mensage(id)
+)
